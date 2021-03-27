21b2e1f9f713898d051c494c942e1b012440446d
AFM - Home <afikrim@deliv.co.id>
2021-03-27 10:44:24 +0700

    spring project init

---
+++ src/test/java/com/github/afikrim/flop/FlopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.afikrim.flop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
